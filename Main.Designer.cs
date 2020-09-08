d2053389656e39c7503969ed2e53557a12776c5c
Albert <47435340+kaninvt@users.noreply.github.com>
2020-09-08 00:52:52 -0400

    Uploaded MiniChat project from local drive to repo

---
+++ Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MiniChat
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.input = new System.Windows.Forms.TextBox();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.serverLabel = new System.Windows.Forms.Label();
+            this.msgInput = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.ListBox();
+            this.attemptConnection = new System.ComponentModel.BackgroundWorker();
+            this.ping = new System.ComponentModel.BackgroundWorker();
+            this.serverMode = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.input.ForeColor = System.Drawing.Color.White;
+            this.input.HideSelection = false;
+            this.input.Location = new System.Drawing.Point(160, 11);
+            this.input.Margin = new System.Windows.Forms.Padding(1);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(153, 23);
+            this.input.TabIndex = 0;
+            this.input.Text = "IP:Port";
+            this.input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.input_KeyDown);
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.connectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.connectBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.connectBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.connectBtn.Location = new System.Drawing.Point(612, 7);
+            this.connectBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(78, 30);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = false;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // serverLabel
+            // 
+            this.serverLabel.AutoSize = true;
+            this.serverLabel.BackColor = System.Drawing.Color.Transparent;
+            this.serverLabel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.serverLabel.ForeColor = System.Drawing.Color.White;
+            this.serverLabel.Location = new System.Drawing.Point(9, 13);
+            this.serverLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.serverLabel.Name = "serverLabel";
+            this.serverLabel.Size = new System.Drawing.Size(149, 18);
+            this.serverLabel.TabIndex = 2;
+            this.serverLabel.Text = "Server Address:";
+            // 
+            // msgInput
+            // 
+            this.msgInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.msgInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.msgInput.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.msgInput.ForeColor = System.Drawing.Color.White;
+            this.msgInput.Location = new System.Drawing.Point(9, 376);
+            this.msgInput.Margin = new System.Windows.Forms.Padding(1);
+            this.msgInput.Multiline = true;
+            this.msgInput.Name = "msgInput";
+            this.msgInput.Size = new System.Drawing.Size(689, 31);
+            this.msgInput.TabIndex = 3;
+            this.msgInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.msgInput_KeyDown);
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.sendBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.sendBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.sendBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.sendBtn.Location = new System.Drawing.Point(699, 376);
+            this.sendBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(81, 31);
+            this.sendBtn.TabIndex = 4;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = false;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.disconnectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.disconnectBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.disconnectBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.disconnectBtn.Location = new System.Drawing.Point(692, 7);
+            this.disconnectBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(89, 30);
+            this.disconnectBtn.TabIndex = 5;
+            this.disconnectBtn.Text = "Disconnect";
+            this.disconnectBtn.UseVisualStyleBackColor = false;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click);
+            // 
+            // console
+            // 
+            this.console.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(153)))), ((int)(((byte)(212)))));
+            this.console.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold);
+            this.console.ForeColor = System.Drawing.Color.White;
+            this.console.FormattingEnabled = true;
+            this.console.ItemHeight = 18;
+            this.console.Location = new System.Drawing.Point(9, 40);
+            this.console.Margin = new System.Windows.Forms.Padding(1);
+            this.console.Name = "console";
+            this.console.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.console.Size = new System.Drawing.Size(773, 328);
+            this.console.TabIndex = 6;
+            // 
+            // attemptConnection
+            // 
+            this.attemptConnection.DoWork += new System.ComponentModel.DoWorkEventHandler(this.attemptConnection_DoWork);
+            // 
+            // ping
+            // 
+            this.ping.WorkerSupportsCancellation = true;
+            this.ping.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ping_DoWork);
+            // 
+            // serverMode
+            // 
+            this.serverMode.AutoSize = true;
+            this.serverMode.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.serverMode.ForeColor = System.Drawing.Color.White;
+            this.serverMode.Location = new System.Drawing.Point(469, 11);
+            this.serverMode.Name = "serverMode";
+            this.serverMode.Size = new System.Drawing.Size(139, 22);
+            this.serverMode.TabIndex = 7;
+            this.serverMode.Text = "Server Mode";
+            this.serverMode.UseVisualStyleBackColor = true;
+            this.serverMode.CheckedChanged += new System.EventHandler(this.serverMode_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(789, 416);
+            this.Controls.Add(this.serverMode);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.disconnectBtn);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.msgInput);
+            this.Controls.Add(this.serverLabel);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(805, 455);
+            this.MinimumSize = new System.Drawing.Size(805, 455);
+            this.Name = "Main";
+            this.Text = "MiniChat by Albert Bregonia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label serverLabel;
+        private System.Windows.Forms.TextBox msgInput;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.ListBox console;
+        private System.ComponentModel.BackgroundWorker attemptConnection;
+        private System.ComponentModel.BackgroundWorker ping;
+        private System.Windows.Forms.CheckBox serverMode;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,197 @@
+﻿//Base GUI
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+//Network
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace MiniChat
+{
+    public partial class Main : Form
+    {
+        TcpClient client;
+        TcpListener server;
+        StreamWriter output;
+        StreamReader scanner;
+        string currentClient;
+
+        public Main(){InitializeComponent();}
+
+        private void serverMode_CheckedChanged(object sender, EventArgs e)
+        {
+            //Implemented for ease of use - ensures that you know when server mode is enabled
+            input.Enabled = !serverMode.Checked;
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            //Attempt Connection when the 'Connect' button is clicked
+            if (server == null || client == null || !client.Connected)
+                attemptConnection.RunWorkerAsync();
+            else
+                MessageBox.Show("A network thread is already running!");
+        }
+
+        private void input_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Attempt Connection when the 'Enter' key is pressed in the input window
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; //Removes the windows alert sound effect
+                if (server == null || client == null || !client.Connected)
+                    attemptConnection.RunWorkerAsync();
+                else
+                    MessageBox.Show("A network thread is already running!");
+            }
+        }
+
+        private void disconnectBtn_Click(object sender, EventArgs e)
+        {
+            //Disconnect from current server if connected
+            if (server != null || (client != null && client.Connected))
+                disconnect();
+            else
+                MessageBox.Show("A network thread is not running!");
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            //Send a string to the server when the 'Send' button is clicked
+            if (client != null && client.Connected)
+                SendMsg();
+            else
+                MessageBox.Show("A network thread is not running!");
+        }
+
+        private void msgInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Send a string to the server when the 'Enter' key is pressed in the input window
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                if (client != null && client.Connected)
+                    SendMsg();
+                else
+                    MessageBox.Show("A network thread is not running!");
+            }
+        }
+
+        private void SendMsg()
+        {
+            /* Behind the scenes, this is what the 'StreamWriter' object automates:
+             * 
+             * int amt = Encoding.ASCII.GetByteCount(msg.Text.Trim() + "\n");
+             * byte[] data = new byte[amt];
+             * data = Encoding.ASCII.GetBytes(msg.Text.Trim() + "\n");
+             * client.GetStream().Write(data, 0, amt);
+             * 
+             * StreamWriter converts the message to bytes and then writes it to the socket
+             */
+            output.WriteLine(msgInput.Text.Trim());
+            UpdateLog(msgInput.Text.Trim());
+            msgInput.Text = "";
+        }
+
+        private void disconnect()
+        {
+            //Clean disconnect
+            if(ping.IsBusy)
+                ping.CancelAsync();
+            MessageBox.Show("Connection Lost");
+            UpdateLog("Disconnected");
+            client.Close();
+            reset();
+        }
+
+        private void reset()
+        {
+            client = null;
+            server = null;
+            input.Enabled = true;
+            serverMode.Enabled = true;
+        }
+
+        private void UpdateLog(String s)
+        {
+            //Message Logging in the Pseudo-Console
+            console.Items.Add(DateTime.Now + "\t" + s);
+        }
+
+        private void attemptConnection_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            //Disable changing settings while attempting a connection
+            input.Invoke((MethodInvoker)delegate { input.Enabled = false; });
+            serverMode.Invoke((MethodInvoker)delegate { serverMode.Enabled = false; });
+            if (serverMode.Checked)
+                try //Generates a new TCP Server on Port 54000 and waits for a client to connect
+                {
+                    server = new TcpListener(IPAddress.Any, 54000);
+                    server.Start();
+                    MessageBox.Show("Server has successfully initialized. Waiting for a client...");
+                    client = server.AcceptTcpClient();
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to establish a server on current network.", "ERROR");
+                    reset();
+                }
+            else
+                try //Background thread to attempt to connect to a server based on the 'input' window's text
+                {
+                    String[] connection = input.Text.Trim().Split(':');
+                    int port;
+                    int.TryParse(connection[1].Trim(), out port);
+                    client = new TcpClient(connection[0].Trim(), port);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid IP Address or Port", "Failed to Establish Network Connection");
+                    reset();
+                }
+            if (client != null && client.Connected)
+            { //Chat setup and notifier
+                output = new StreamWriter(client.GetStream(), Encoding.ASCII);
+                scanner = new StreamReader(client.GetStream(), Encoding.ASCII);
+                output.AutoFlush = true;
+                console.Invoke((MethodInvoker)delegate
+                {
+                    if (serverMode.Checked)
+                    {
+                        currentClient = ((IPEndPoint)client.Client.RemoteEndPoint).ToString();
+                        UpdateLog(currentClient + " has successfully connected");
+                        console.TopIndex = console.Items.Count - 1;
+                    }
+                    else
+                    {
+                        currentClient = input.Text.Trim();
+                        UpdateLog("Successfully connected to: " + input.Text.Trim());
+                    } 
+                });
+                ping.RunWorkerAsync(); //Start Message Handler
+            }
+        }
+
+        private void ping_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            int online;
+            while (client != null && client.Connected && (online = client.GetStream().ReadByte())>-1)
+            { //Display incoming messages while connection is valid
+                console.Invoke((MethodInvoker)delegate
+                {
+                    UpdateLog("@" + currentClient + " " + (char)online + scanner.ReadLine());
+                    console.TopIndex = console.Items.Count - 1;
+                });
+            }
+            //Disconnect upon connection lost
+            console.Invoke((MethodInvoker)delegate 
+            {
+                disconnect();
+                console.TopIndex = console.Items.Count - 1;
+            });
+            ping.CancelAsync(); //End Message Handler
+        }
+    }
+}
